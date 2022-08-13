5073fb74e94548754951761d3ef72f85aaf13021
rafsaf <rafal.safin12@gmail.com>
2022-08-13 12:23:53 +0200

    resolve some exit multithreading problems

--- pg_dump/main.py
+++ pg_dump/main.py
@@ -83,12 +83,15 @@ class PgDumpDaemon:
 
     def exit(self, sig, frame):
         self.scheduler_thread.stop()
+        self.scheduler_thread.join()
         for thread in self.pgdump_threads:
             thread.stop()
-
+        for thread in self.pgdump_threads:
+            thread.join()
         with open(settings.PGDUMP_PICKLE_PGDUMP_QUEUE_NAME, "wb") as file:
             pickle.dump(list(core.PGDUMP_QUEUE.queue), file)
         log.info("Saved pickled PGDUMP_QUEUE to file")
+        log.info("PgDumpDaemon exits gracefully")
 
 
 if __name__ == "__main__":
--- pg_dump/pgdump_thread.py
+++ pg_dump/pgdump_thread.py
@@ -47,9 +47,9 @@ class PgDumpThread(Thread):
                     "Pgdump thread job started at %s has exceeded max number of retries"
                 )
                 continue
+            path = core.backup_folder_path(self.job.filename)
             try:
                 core.run_pg_dump(self.job.filename)
-                path = core.backup_folder_path(self.job.filename)
                 if path.exists() and not path.stat().st_size:
                     log.error("Error pgdump thread %s: backup file empty", self.number)
                     raise core.CoreSubprocessError()
@@ -58,10 +58,14 @@ class PgDumpThread(Thread):
                 log.error(
                     "Error pgdump thread %s: error performing pgdump", self.number
                 )
-                core.backup_folder_path(self.job.filename).unlink()
-                log.error("Removed empty backup file %s", self.job.filename)
+                if path.exists():
+                    core.backup_folder_path(self.job.filename).unlink()
+                    log.error("Removed empty backup file %s", self.job.filename)
                 self.cooling_period()
                 self.job.retries += 1
+                log.error(
+                    "Adding job back to PGDUMP_QUEUE after error: %s", self.job.filename
+                )
                 core.PGDUMP_QUEUE.put(self.job)
         log.info("Pgdump thread %s has stopped", self.number)
 
@@ -75,13 +79,14 @@ class PgDumpThread(Thread):
             self.number,
             release_time,
         )
-        while self.running:
+        while self.running():
             now = datetime.utcnow()
             if now > release_time:
-                log.info("Pgdump thread %s finished cooling period", self.number)
                 self.cooling = False
+                log.info("Pgdump thread %s finished cooling period", self.number)
                 return
             time.sleep(0.02)
+        log.info("Pgdump thread %s skipping cooling period", self.number)
 
     def stop(self):
         log.info("Stopping pgdump thread %s", self.number)
