70cc2ce2fb7714327fc3b3034ce5eb0490b07d83
rafsaf <rafal.safin12@gmail.com>
2023-04-24 02:03:09 +0200

    fix local.py provider to always zip files

--- backuper/storage_providers/local.py
+++ backuper/storage_providers/local.py
@@ -1,8 +1,8 @@
 import logging
-import os
+import shutil
 from pathlib import Path
 
-from backuper import config
+from backuper import config, core
 from backuper.storage_providers import base_provider
 
 log = logging.getLogger(__name__)
@@ -17,19 +17,31 @@ class LocalFiles(base_provider.BaseBackupProvider):
     NAME = config.BackupProviderEnum.LOCAL_FILES
 
     def _post_save(self, backup_file: Path):
+        try:
+            core.run_create_zip_archive(backup_file=backup_file)
+        except core.CoreSubprocessError:
+            log.error("could not create zip_backup_file from %s", backup_file)
+            raise
         return True
 
     def _clean(self, backup_file: Path):
+        if backup_file.is_file():
+            backup_file.unlink()
+        else:
+            shutil.rmtree(backup_file)
         files: list[str] = []
         for backup_path in backup_file.parent.iterdir():
             files.append(str(backup_path.absolute()))
         files.sort(reverse=True)
         while len(files) > config.BACKUP_MAX_NUMBER:
-            backup_to_remove = files.pop()
+            backup_to_remove = Path(files.pop())
             try:
-                os.unlink(backup_to_remove)
-                log.info("removed file %s", backup_to_remove)
+                if backup_to_remove.is_file():
+                    backup_to_remove.unlink()
+                else:
+                    shutil.rmtree(backup_to_remove)
+                log.info("removed path %s", backup_to_remove)
             except Exception as e:
                 log.error(
-                    "could not remove file %s: %s", backup_to_remove, e, exc_info=True
+                    "could not remove path %s: %s", backup_to_remove, e, exc_info=True
                 )
