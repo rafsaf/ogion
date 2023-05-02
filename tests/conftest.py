c03806317a5683dbf362b3ec58f3dec48ebc663b
rafsaf <rafal.safin12@gmail.com>
2023-05-02 12:27:38 +0200

    fix tests logging configuration

--- tests/conftest.py
+++ tests/conftest.py
@@ -153,6 +153,9 @@ def fixed_config_setup(tmp_path: Path, monkeypatch: MonkeyPatch):
     monkeypatch.setattr(config, "BACKUP_MAX_NUMBER", 1)
     monkeypatch.setattr(config, "ZIP_ARCHIVE_PASSWORD", "test")
     monkeypatch.setattr(config, "GOOGLE_BUCKET_UPLOAD_PATH", "test")
+    CONST_LOG_FOLDER_PATH = tmp_path / "pytest_logs"
+    CONST_LOG_FOLDER_PATH.mkdir()
+    monkeypatch.setattr(config, "CONST_LOG_FOLDER_PATH", CONST_LOG_FOLDER_PATH)
     CONST_BACKUP_FOLDER_PATH = tmp_path / "pytest_data"
     monkeypatch.setattr(config, "CONST_BACKUP_FOLDER_PATH", CONST_BACKUP_FOLDER_PATH)
     CONST_PGPASS_FILE_PATH = tmp_path / "pytest_pgpass"
