d0e1d5dd6827cd2b7ba1e1e7f62e728f2bacfe5b
rafsaf <rafal.safin12@gmail.com>
2023-07-11 02:45:23 +0200

    move to init subclass in provider

--- backuper/backup_targets/base_target.py
+++ backuper/backup_targets/base_target.py
@@ -10,8 +10,6 @@ log = logging.getLogger(__name__)
 
 
 class BaseBackupTarget(ABC):
-    NAME = "target"
-
     def __init__(self, cron_rule: str, env_name: str) -> None:
         self.cron_rule: str = cron_rule
         self.env_name = env_name
