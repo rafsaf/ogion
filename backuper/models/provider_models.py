124ea4019ef3ff628cdb59ba5b21bac95c23ecd6
rafsaf <rafal.safin12@gmail.com>
2023-07-21 14:25:07 +0200

    shorten process_service_account_base64

--- backuper/models/provider_models.py
+++ backuper/models/provider_models.py
@@ -1,5 +1,4 @@
 import base64
-import os
 
 from pydantic import BaseModel, field_validator
 
@@ -21,10 +20,5 @@ class GCSProviderModel(ProviderModel):
 
     @field_validator("service_account_base64")
     def process_service_account_base64(cls, service_account_base64: str) -> str:
-        service_account_bytes = base64.b64decode(service_account_base64)
-        with open(config.CONST_GOOGLE_SERVICE_ACCOUNT_PATH, "wb") as f:
-            f.write(service_account_bytes)
-        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = str(
-            config.CONST_GOOGLE_SERVICE_ACCOUNT_PATH
-        )
+        base64.b64decode(service_account_base64)
         return service_account_base64
