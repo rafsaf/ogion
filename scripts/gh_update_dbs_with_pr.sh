40cd804072bec9827d24c487346471976daa3efe
rafsaf <rafal.safin12@gmail.com>
2023-10-15 19:18:34 +0200

    fix update script

--- scripts/gh_update_dbs_with_pr.sh
+++ scripts/gh_update_dbs_with_pr.sh
@@ -12,12 +12,11 @@ else
     echo "file changed"
     timestamp=$(date +%s)
     branch="update-docker-compose-dbs-$timestamp"
-    msg="'update docker-compose.dbs.yml file'"
     git config user.name "GitHub Actions Bot"
     git config user.email "<>"
     git status
     git branch $branch
-    git commit -am $msg
+    git commit -am 'update docker-compose.dbs.yml file'
     git push -u origin $branch
-    gh pr create -B main -H $branch --title $msg --body 'Created by Github action'
+    gh pr create -B main -H $branch --title 'update docker-compose.dbs.yml file' --body 'Created by Github action'
 fi
