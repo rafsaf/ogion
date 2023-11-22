7eb1a5085b0393f4fc944367c6552fd1cfcba8c7
rafsaf <rafal.safin12@gmail.com>
2023-11-22 22:34:33 +0100

    gh compose dbs action auto create pr and merge

---
+++ scripts/gh_update_dbs_pr.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+DB_COMPOSE_FILE="docker/docker-compose.dbs.yml"
+
+git config user.name "GitHub Actions Bot"
+git config user.email "<>"
+git status
+DIFF=$(git diff origin/main $DB_COMPOSE_FILE)
+if [ "$DIFF" = "" ]
+then
+    echo "no changes detected"
+else
+    echo "file changed"
+    timestamp=$(date +%s)
+    branch="update-docker-compose-dbs-$timestamp"
+    git checkout -b $branch
+    git add .
+    git commit -m 'GitHub Actions Bot: updated docker-compose.dbs.yml file'
+    git push -u origin $branch
+    gh pr create -B main -H $branch --title 'update docker-compose.dbs.yml file' --body 'Created by Github action'
+    gh pr merge $branch --auto
+fi
