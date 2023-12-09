c1dc67260d61fc2553702c032ab9ce80f09736b0
rafsaf <rafal.safin12@gmail.com>
2023-12-09 22:41:48 +0100

    fix problem in update script, run smoke tests inside

--- scripts/gh_update_dbs_pr.sh
+++ scripts/gh_update_dbs_pr.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
-DB_COMPOSE_FILE="docker/docker-compose.dbs.yml"
+# To be run in github action update_compose_dbs.yml
 
 git config user.name "GitHub Actions Bot"
 git config user.email "<>"
 git status
-DIFF=$(git diff origin/main $DB_COMPOSE_FILE)
+DIFF=$(git diff origin/main)
 if [ "$DIFF" = "" ]
 then
     echo "no changes detected"
 else
     echo "file changed"
+    make tests_amd64
+    echo "merge changes"
     timestamp=$(date +%s)
     branch="update-docker-compose-dbs-$timestamp"
     git checkout -b $branch
