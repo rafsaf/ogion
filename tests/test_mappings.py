8a5d8c5b6fdda3fe070515b5ca81d7259be1c504
Rafał Safin <rafal.safin@rafsaf.pl>
2024-03-14 14:34:06 +0100

    add test_all_mappings_value_classes_are_unique
    Signed-off-by: Rafał Safin <rafal.safin@rafsaf.pl>

--- tests/test_mappings.py
+++ tests/test_mappings.py
@@ -1,3 +1,5 @@
+from collections.abc import Callable
+from typing import Any
 from backuper.config import BackupTargetEnum, UploadProviderEnum
 from backuper.models.models_mapping import get_target_map, get_provider_map
 from backuper.backup_targets.targets_mapping import get_target_cls_map
@@ -5,6 +7,7 @@ from backuper.upload_providers.providers_mapping import get_provider_cls_map
 from backuper.backup_targets.base_target import BaseBackupTarget
 from backuper.upload_providers.base_provider import BaseUploadProvider
 from backuper.models import upload_provider_models, backup_target_models
+import pytest
 
 
 def test_get_target_map_contains_all_enums_and_has_valid_value_types() -> None:
@@ -45,3 +48,14 @@ def test_get_provider_cls_map_contains_all_enums_and_has_valid_value_types() -> 
 
     for cls in mapping.values():
         assert issubclass(cls, BaseUploadProvider)
+
+
+@pytest.mark.parametrize(
+    "mapping_func",
+    [get_target_map, get_target_cls_map, get_provider_map, get_provider_cls_map],
+)
+def test_all_mappings_value_classes_are_unique(
+    mapping_func: Callable[[], dict[str, Any]]
+) -> None:
+    values = list(mapping_func().values())
+    assert len(values) == len(set(values))
