f33a7c07ff2533930331bd8d34746329fafc18f1
Rafał Safin <51059348+rafsaf@users.noreply.github.com>
2025-01-28 18:38:08 +0100

    bump and small fix to update script (#360)
    Signed-off-by: rafsaf <rafal.safin@rafsaf.pl>

--- scripts/gh_update_dbs_pr.sh
+++ scripts/gh_update_dbs_pr.sh
@@ -20,5 +20,5 @@ else
     git commit -m 'GitHub Actions Bot: updated docker-compose.dbs.yml file'
     git push -u origin $branch
     gh pr create -B main -H $branch --title 'update docker-compose.dbs.yml file' --body 'Created by Github action' --label 'auto-update'
-    gh pr merge $branch --squash --admin --delete-branch
+    gh pr merge $branch --squash --admin
 fi
