69898ce22019888ca7f6be8564b91a6d1dae985f
Rafał Safin <rafal.safin@rafsaf.pl>
2025-10-04 19:46:16 +0200

    use postgresql-client-18, fix memory leak
    Signed-off-by: Rafał Safin <rafal.safin@rafsaf.pl>

--- scripts/install_postgresql_client.sh
+++ scripts/install_postgresql_client.sh
@@ -8,7 +8,7 @@
 CPU="$(dpkg --print-architecture)"
 DISTR_VERSION="$(awk -F= '/^VERSION_CODENAME=/{print $2}' /etc/os-release)"
 
-echo "Installing postgresql-client-17"
+echo "Installing postgresql-client-18"
 
 mkdir -p /etc/apt/keyrings/
 if [ -f "/etc/apt/keyrings/www.postgresql.org.gpg" ]
@@ -23,5 +23,5 @@ then
 else
   echo "deb [signed-by=/etc/apt/keyrings/www.postgresql.org.gpg arch=$CPU] http://apt.postgresql.org/pub/repos/apt $DISTR_VERSION-pgdg main" > /etc/apt/sources.list.d/pgdg.list
 fi
-apt-get -y update && apt-get -y install postgresql-client-17
-echo "postgresql-client-17 installed"
+apt-get -y update && apt-get -y install postgresql-client-18
+echo "postgresql-client-18 installed"
