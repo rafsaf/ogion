36917fa1b421888940d0110f827d4d7b15b42f89
Rafał Safin <51059348+rafsaf@users.noreply.github.com>
2026-03-13 20:41:56 +0100

    Bump and minor dev fixes (#454)
    * Bump and add --skip maxscale to mariadb installation plus ignore too old api version on azurite
    
    Signed-off-by: rafsaf <rafal.safin12@gmail.com>
    
    * bump ruff in pre-commit
    
    Signed-off-by: rafsaf <rafal.safin12@gmail.com>
    
    * run update_compose_db_file
    
    Signed-off-by: rafsaf <rafal.safin12@gmail.com>
    
    ---------
    
    Signed-off-by: rafsaf <rafal.safin12@gmail.com>

--- scripts/install_mariadb_client.sh
+++ scripts/install_mariadb_client.sh
@@ -5,6 +5,7 @@
 # https://mariadb.com/kb/en/mariadb-package-repository-setup-and-usage/
 #########################################################################
 echo "Installing mariadb-client with ready script"
-curl -LsS https://r.mariadb.com/downloads/mariadb_repo_setup | bash
+curl -LsS https://r.mariadb.com/downloads/mariadb_repo_setup | \
+	bash -s -- --skip-maxscale
 apt-get -y update && apt-get -y install mariadb-client
 echo "mariadb-client installed"
